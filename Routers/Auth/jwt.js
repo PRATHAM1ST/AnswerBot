a2cccba6f3dd518077ee9bf09ff09049247326fc
Pratham <chudasamapratham142@gmail.com>
2023-03-18 22:51:10 +0530

    jwt token fixed

--- Routers/Auth/jwt.js
+++ Routers/Auth/jwt.js
@@ -8,28 +8,24 @@ const { v4: uuidv4 } = require("uuid");
 // Creating a secret key
 const secret = process.env.JWT_SECRET_KEY;
 
-// Check for authoririty
+// Check for authority
 router.use((req, res, next) => {
-	const token = req.cookies.token;
-	if (!token) {
-		// Generate the JWT
-		const token = jwt.sign({ id: uuidv4() }, secret);
+  const token = req.cookies.token;
+  if (!token) {
+    // Generate the JWT
+    const newToken = jwt.sign({ id: uuidv4() }, secret);
 
-		// Set the JWT as a cookie
-		res.cookie("token", token, { httpOnly: true });
-		return res.redirect(302, req.originalUrl);
-	}
-	try {
-		const decoded = jwt.verify(token, secret);
-		req.user = decoded;
-		next();
-	} catch (err) {
-		return res.status(401).send({ message: "Invalid token" });
-	}
-});
-
-router.get("/verifyJWT", (req, res) => {
-	res.status(200).send({ message: "JWT verified", user: req.user });
+    // Set the JWT as a cookie
+    res.cookie("token", newToken, { httpOnly: true });
+  } else {
+    try {
+      const decoded = jwt.verify(token, secret);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+  }
+  next();
 });
 
 module.exports = router;
